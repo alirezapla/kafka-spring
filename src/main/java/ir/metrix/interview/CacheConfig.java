5fe166d8e7d0ad4dfa0f7844dda34bf448f06af2
pla <adad@asdasd.csd>
2024-08-19 18:43:13 +0330

    feat: add cahcing layer

--- src/main/java/ir/metrix/interview/CacheConfig.java
+++ src/main/java/ir/metrix/interview/CacheConfig.java
@@ -1,2 +1,34 @@
-package ir.metrix.interview;public class CacheConfig {
+package ir.metrix.interview;
+
+
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+
+    @Value("${cache.ttl}")
+    private int ttl;
+    @Value("${cache.name}")
+    private String name;
+
+    @Bean
+    public HazelcastInstance hazelcastInstance() {
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance();
+        instance.getConfig().addMapConfig(new MapConfig(name).setTimeToLiveSeconds(ttl));
+        return instance;
+    }
+
+    @Bean
+    public ClientConfig clientConfig() {
+        ClientConfig cfg = ClientConfig.load();
+        cfg.setClusterName("phoneCluster");
+        return cfg;
+    }
+
 }
