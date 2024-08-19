93ab69cb1bbbaadebcbea8926412110357700218
pla <adad@asdasd.csd>
2024-08-19 18:44:23 +0330

    fix: add partitions for topic

--- src/main/java/ir/metrix/interview/InterviewApplication.java
+++ src/main/java/ir/metrix/interview/InterviewApplication.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
+@EnableCaching
 @EnableKafka
 @SpringBootApplication
 public class InterviewApplication {
@@ -25,9 +27,10 @@ public class InterviewApplication {
 	private int messageCount;
 
 	@Bean
-	public NewTopic topic() { //TODO: Adjust topic configuration like partition and replica
+	public NewTopic topic() {
 		return TopicBuilder
 				.name(topicName)
+				.partitions(5)
 				.build();
 	}
 
@@ -35,7 +38,7 @@ public class InterviewApplication {
 	public ApplicationRunner runner(KafkaTemplate<String, Message> template) {
 		return args -> {
 			int count = messageCount;
-			for (int i = 0; i < count; i++)  //we have only 2 users
+			for (int i = 0; i < count; i++)
 				template.send(topicName, new Message("user"+(i%2), "just a message!"));
 		};
 	}
