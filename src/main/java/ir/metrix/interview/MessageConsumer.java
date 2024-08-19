1568bb5772558e04ddf58d1ed34b2e86ac1cf244
pla <adad@asdasd.csd>
2024-08-19 18:44:51 +0330

    fix: enable batch processing

--- src/main/java/ir/metrix/interview/MessageConsumer.java
+++ src/main/java/ir/metrix/interview/MessageConsumer.java
@@ -5,10 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-//TODO: maximise the consumer performance in terms of how many message it can process per second
 @Component
 public class MessageConsumer {
     private static final Logger LOG = LoggerFactory.getLogger(MessageConsumer.class.getName());
@@ -19,14 +19,17 @@ public class MessageConsumer {
         this.service = service;
     }
 
-    //TODO: metric for throughput (to show how many message per seconds we can handle)
     @KafkaListener(
-            topics = "${topic.name}"
-    )  //TODO: Adjust kafka listener configuration
-    public void consume(Message message) {  //TODO: use batch poll
-
-        this.service.processMessage(message);
-
+            topics = "${topic.name}",
+            batch = "true",
+            concurrency = "5"
+    )
+    public void consume(List<Message> messages) {
+        LOG.info(String.valueOf(messages.size()));
+        this.service.processMessage(messages);
     }
 
+
 }
+
+
