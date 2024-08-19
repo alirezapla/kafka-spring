b772557eabb2d239616d21ed9cace428f8cd3373
pla <adad@asdasd.csd>
2024-08-19 18:44:58 +0330

    feat: add caching layer

--- src/main/java/ir/metrix/interview/MessageProcessorService.java
+++ src/main/java/ir/metrix/interview/MessageProcessorService.java
@@ -5,37 +5,36 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Service
 public class MessageProcessorService {
 
     private static final Logger LOG = LoggerFactory.getLogger(MessageProcessorService.class.getName());
-
-    @Value("${message.count}")
-    private int messageCount;
     private final AtomicInteger processedCount = new AtomicInteger(0);
     private final Object lock = new Object();
-
     private final UserService userService;
     private final SmsService smsService;
+    @Value("${message.count}")
+    private int messageCount;
 
     MessageProcessorService(UserService userService, SmsService smsService) {
         this.userService = userService;
         this.smsService = smsService;
     }
 
-    public void processMessage(Message message) {
-        String phoneNumber = userService.fetchPhoneById(message.getUserId());
-        smsService.sendMessage(phoneNumber, message);
-
-        synchronized (lock) {  //TODO: remove later
-            if (processedCount.get() == 0)
-                LOG.info("Start Processing...");
-
-            if (processedCount.incrementAndGet() >= messageCount)
-                LOG.info("Finish Processing Messages. count={}", processedCount);
+    public void processMessage(List<Message> messages) {
+        Map<String, List<Message>> messagesGroupedByUserId = messages.stream().collect(Collectors.groupingBy(Message::getUserId));
+        for (var entry : messagesGroupedByUserId.entrySet()) {
+            var phoneNumber = userService.fetchPhoneById(entry.getKey());
+            smsService.sendMessagesAsync(phoneNumber, entry.getValue());
         }
+        LOG.info("Processing Finished");
     }
 
 }
--- src/main/java/ir/metrix/interview/UserService.java
+++ src/main/java/ir/metrix/interview/UserService.java
@@ -1,11 +1,14 @@
 package ir.metrix.interview;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Cacheable(cacheNames = "phone", key = "#userId")
     public String fetchPhoneById(String userId) {
+        System.out.println("fetchPhoneById called");
         try {
             Thread.sleep(500);  //0.5s
             return userId;
